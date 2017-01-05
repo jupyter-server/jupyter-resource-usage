8cb4f5d8879c573a4fe690c4f53c2b0a99d18d69
Brian E. Granger <ellisonbg@gmail.com>
2017-01-05 11:44:14 -0700

    Handle case of memory limit not set.

--- nbresuse/handlers.py
+++ nbresuse/handlers.py
@@ -8,10 +8,13 @@ def get_metrics():
     cur_process = psutil.Process()
     all_processes = [cur_process] + cur_process.children(recursive=True)
     rss = sum([p.memory_info().rss for p in all_processes])
+    mem_limit = os.environ.get('MEM_LIMIT', None)
+    if mem_limit is not None:
+        mem_limit = int(mem_limit)
     return {
         'rss': rss,
         'limits': {
-            'memory': int(os.environ.get('MEM_LIMIT', None))
+            'memory': mem_limit
         }
     }
 
