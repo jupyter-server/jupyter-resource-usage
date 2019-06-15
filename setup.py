e7b723b93c2065d067394042968208c8a6dd7d9d
yuvipanda <yuvipanda@gmail.com>
2019-06-14 21:01:55 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="nbresuse",
-    version='0.3.1',
+    version='0.3.2',
     url="https://github.com/yuvipanda/nbresuse",
     author="Yuvi Panda",
     description="Simple Jupyter extension to show how much resources (RAM) your notebook is using",
