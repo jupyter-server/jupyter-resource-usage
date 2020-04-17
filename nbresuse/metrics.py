63117fb68f7450c246553929ee4071740d981e6e
William Krinsman <krinsman@berkeley.edu>
2020-04-17 12:26:08 -0700

    Make psutil optional

--- nbresuse/metrics.py
+++ nbresuse/metrics.py
@@ -1,6 +1,9 @@
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
-import psutil
+try:
+    import psutil
+except ImportError:
+    psutil = None
 
 
 class MemoryMetrics(NamedTuple):
@@ -13,30 +16,38 @@ class CPUMetrics(NamedTuple):
     cpu_usage: float
 
 
-def memory_metrics() -> MemoryMetrics:
-    cur_process = psutil.Process()
-    all_processes = [cur_process] + cur_process.children(recursive=True)
+def memory_metrics() -> Optional[MemoryMetrics]:
+    if psutil:
+        cur_process = psutil.Process()
+        all_processes = [cur_process] + cur_process.children(recursive=True)
 
-    rss = sum([p.memory_info().rss for p in all_processes])
-    virtual_memory = psutil.virtual_memory()
+        rss = sum([p.memory_info().rss for p in all_processes])
+        virtual_memory = psutil.virtual_memory().total
 
-    return MemoryMetrics(rss, virtual_memory.total)
+    else:
+        return None
+
+    return MemoryMetrics(rss, virtual_memory)
 
 
-def cpu_metrics() -> CPUMetrics:
-    cur_process = psutil.Process()
-    all_processes = [cur_process] + cur_process.children(recursive=True)
+def cpu_metrics() -> Optional[CPUMetrics]:
+    if psutil:
+        cur_process = psutil.Process()
+        all_processes = [cur_process] + cur_process.children(recursive=True)
 
-    cpu_count = psutil.cpu_count()
+        cpu_count = psutil.cpu_count()
 
-    def get_cpu_percent(p):
-        try:
-            return p.cpu_percent(interval=0.05)
-        # Avoid littering logs with stack traces complaining
-        # about dead processes having no CPU usage
-        except BaseException:
-            return 0
+        def get_cpu_percent(p):
+            try:
+                return p.cpu_percent(interval=0.05)
+            # Avoid littering logs with stack traces complaining
+            # about dead processes having no CPU usage
+            except BaseException:
+                return 0
 
-    cpu_percent = sum([get_cpu_percent(p) for p in all_processes])
+        cpu_percent = sum([get_cpu_percent(p) for p in all_processes])
+
+    else:
+        return None
 
     return CPUMetrics(cpu_count * 100.0, cpu_percent)
