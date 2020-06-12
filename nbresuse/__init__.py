ef69cad1175fa92543fce085cd46a9ec990fa55b
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-06-12 08:49:02 +0200

    Add back the /metrics endpoint

--- nbresuse/__init__.py
+++ nbresuse/__init__.py
@@ -34,8 +34,14 @@ def load_jupyter_server_extension(nbapp):
     """
     resuseconfig = ResourceUseDisplay(parent=nbapp)
     nbapp.web_app.settings["nbresuse_display_config"] = resuseconfig
-    route_pattern = url_path_join(nbapp.web_app.settings['base_url'], '/api/nbresuse/v1')
-    nbapp.web_app.add_handlers('.*', [(route_pattern, ApiHandler)])
+    base_url = nbapp.web_app.settings["base_url"]
+    nbapp.web_app.add_handlers(
+        ".*",
+        [
+            (url_path_join(base_url, "/api/nbresuse/v1"), ApiHandler),
+            (url_path_join(base_url, "/metrics"), ApiHandler),
+        ],
+    )
     callback = ioloop.PeriodicCallback(
         PrometheusHandler(PSUtilMetricsLoader(nbapp)), 1000
     )
