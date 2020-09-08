72a3ba1cbb899a7d84348c3b8a9807ec474a6914
Slaytanic <horseman@163.com>
2020-09-08 15:13:10 +0800

    Update api.py

--- nbresuse/api.py
+++ nbresuse/api.py
@@ -29,30 +29,27 @@ class ApiHandler(IPythonHandler):
         all_processes = [cur_process] + cur_process.children(recursive=True)
 
         # Get memory information
+        # If running in a docker container or pod
         dockerized = self.__if_dockerized()
         if not dockerized:
             # if running on physical server
             rss = sum([p.memory_info().rss for p in all_processes])
-            # unused rss, use system avail memory instead
-        else:
-            # if running in docker container or pod
-            with open('/sys/fs/cgroup/memory/memory.usage_in_bytes') as usage:
-                rss = int(usage.read().strip())
-                
-        if not dockerized:
             if callable(config.mem_limit):
                 mem_limit = config.mem_limit(rss=rss)
             else:  # mem_limit is an Int
                 mem_limit = config.mem_limit
+            # unused rss, use system avail memory instead
         else:
-            # for docker use only
-            with open('/sys/fs/cgroup/memory/memory.limit_in_bytes') as limit:
-                mem_limit = int(limit.read().strip())
+            # if running in docker container or pod
+            mem_limit = self.__get_docker_physical_memory()
+            rss = self.__get_docker_memory_usage()
+            real_rss = self.__get_docker_real_rss_memory()
+            rss = rss - real_rss
 
         limits = {"memory": {"rss": mem_limit}}
-        if config.mem_limit and config.mem_warning_threshold != 0:
+        if config.mem_limit:
             limits["memory"]["warn"] = (mem_limit - rss) < (
-                mem_limit * config.mem_warning_threshold
+                    mem_limit * config.mem_warning_threshold
             )
 
         metrics = {"rss": rss, "limits": limits}
@@ -72,7 +69,7 @@ class ApiHandler(IPythonHandler):
                 limits["cpu"] = {"cpu": config.cpu_limit}
                 if config.cpu_warning_threshold != 0:
                     limits["cpu"]["warn"] = (config.cpu_limit - cpu_percent) < (
-                        config.cpu_limit * config.cpu_warning_threshold
+                            config.cpu_limit * config.cpu_warning_threshold
                     )
 
             metrics.update(cpu_percent=cpu_percent, cpu_count=cpu_count)
@@ -88,9 +85,49 @@ class ApiHandler(IPythonHandler):
             # about dead processes having no CPU usage
             except:
                 return 0
-
         return sum([get_cpu_percent(p) for p in all_processes])
-    
+
+    @staticmethod
+    def __get_docker_physical_memory():
+        with open('/sys/fs/cgroup/memory/memory.limit_in_bytes') as limit:
+            mem_limit = int(limit.read().strip())
+        return mem_limit
+
+    @staticmethod
+    def __get_docker_memory_usage():
+        with open('/sys/fs/cgroup/memory/memory.usage_in_bytes') as usage:
+            all_used = int(usage.read().strip())
+        return all_used
+
+    @staticmethod
+    def __get_docker_real_rss_memory():
+        real_rss = 0
+        with open('/sys/fs/cgroup/memory/memory.stat') as stats:
+            for line in stats.readlines():
+                stat = line.split()
+                if stat[0] in ['rss', 'inactive_file', 'active_file']:
+                    real_rss = real_rss + int(stat[1])
+        return real_rss
+
+    def _check_mem_used(self):
+        memory_limit = 20 * 1024 * 1024 * 1024
+        sysmem = self.__get_docker_physical_memory()
+        rss = self.__get_docker_memory_usage()
+
+        real_rss = self.__get_docker_real_rss_memory()
+
+        avail_mem = rss - real_rss
+        if sysmem < memory_limit:
+            if round(avail_mem / sysmem * 100) < 20:
+                return True
+            else:
+                return False
+        else:
+            if avail_mem < 5 * 1024 * 1024 * 1024:
+                return True
+            else:
+                return False
+
     @staticmethod
     def __if_dockerized():
         if os.path.exists('/.dockerenv'):
@@ -99,3 +136,4 @@ class ApiHandler(IPythonHandler):
         else:
             # running on physical server
             return False
+
