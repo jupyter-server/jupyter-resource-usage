86d87a9eca14505fc83e94f2658e24d245c7a4cd
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-11-18 22:31:10 +0100

    Add a lab extension to display memory usage
    Co-authored-by: RichaGadgil <gadgilricha@gmail.com>
    Co-authored-by: Declan Kelly <dkelly.home@gmail.com>

--- setup.py
+++ setup.py
@@ -1,27 +1,58 @@
-import pathlib
+import os
 from glob import glob
 
 import setuptools
+from jupyter_packaging import combine_commands
+from jupyter_packaging import create_cmdclass
+from jupyter_packaging import ensure_targets
+from jupyter_packaging import get_version
+from jupyter_packaging import install_npm
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+HERE = os.path.abspath(os.path.dirname(__file__))
 
-# The text of the README file
-README = (HERE / "README.md").read_text()
+# The name of the project
+name = "jupyter-resource-usage"
+
+src_path = os.path.join(HERE, "packages", "labextension")
+lab_path = os.path.join(HERE, name, "labextension")
+
+# Representative files that should exist after a successful build
+jstargets = [
+    os.path.join(src_path, "lib", "index.js"),
+    os.path.join(lab_path, "package.json"),
+]
+
+package_data_spec = {name: ["*"]}
+
+labext_name = "@jupyter-server/resource-usage"
+
+data_files_spec = [
+    ("share/jupyter/labextensions/%s" % labext_name, lab_path, "**"),
+    ("share/jupyter/labextensions/%s" % labext_name, HERE, "install.json"),
+]
+
+cmdclass = create_cmdclass(
+    "jsdeps", package_data_spec=package_data_spec, data_files_spec=data_files_spec
+)
+
+cmdclass["jsdeps"] = combine_commands(
+    install_npm(src_path, build_cmd="build:prod", npm=["jlpm"]),
+    ensure_targets(jstargets),
+)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
-    name="jupyter-resource-usage",
+    name=name,
     version="0.4.0",
     url="https://github.com/jupyter-server/jupyter-resource-usage",
     author="Jupyter Development Team",
     description="Simple Jupyter extension to show how much resources (RAM) your notebook is using",
-    long_description=README,
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    license="BSD",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-    ],
+    cmdclass=cmdclass,
     packages=setuptools.find_packages(),
     install_requires=["notebook>=5.6.0", "prometheus_client", "psutil>=5.6.0"],
     extras_require={
@@ -43,4 +74,9 @@ setuptools.setup(
     ],
     zip_safe=False,
     include_package_data=True,
+    license="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+    ],
 )
