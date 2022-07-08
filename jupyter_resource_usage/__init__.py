20b6de6aa1217e207e736761326136919c778853
casparl <caspar.vanleeuwen@surf.nl>
2022-07-08 18:30:53 +0200

    Fix https://github.com/jupyter-server/jupyter-resource-usage/issues/91 and define _load_jupyter_server_extension for further backwards compatibility (see https://jupyter-server.readthedocs.io/en/latest/developers/extensions.html#migrating-an-extension-to-use-jupyter-server)

--- jupyter_resource_usage/__init__.py
+++ jupyter_resource_usage/__init__.py
@@ -34,5 +34,6 @@ def _jupyter_nbextension_paths():
         }
     ]
 
-
-load_jupyter_server_extension = load_jupyter_server_extension
+# For backwards compatibility
+_load_jupyter_server_extension = load_jupyter_server_extension
+_jupyter_server_extension_paths = _jupyter_server_extension_points
